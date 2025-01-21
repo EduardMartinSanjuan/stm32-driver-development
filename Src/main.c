1d4b9707eb3da24bbc7cf1e7a3c2db9fd8c1b805
EduardMartinSanjuan <eduard.martin.sanjuan2@gmail.com>
2025-01-21 15:12:32 +0100

    First commit

--- Src/main.c
+++
@@ -1,9 +0,0 @@
-
-#include "stm32f407xx.h"
-#include <stdint.h>
-
-int main(void)
-{
-    /* Loop forever */
-	for(;;);
-}
--- drivers/Src/stm32f407xx_gpio_driver.c
+++ drivers/Src/stm32f407xx_gpio_driver.c
@@ -1,2 +1,288 @@
 #include "stm32f407xx_gpio_driver.h"
+#include "stm32f407xx.h"
 
+
+/*********************************************************************
+ * @fn      		  - GPIO_PeriClockControl
+ *
+ * @brief             - This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]         - base address of the gpio peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		if(pGPIOx == GPIOA)
+		{
+			GPIOA_PCLK_EN();
+		}else if(pGPIOx == GPIOB)
+		{
+			GPIOB_PCLK_EN();
+		}else if(pGPIOx == GPIOC)
+		{
+			GPIOC_PCLK_EN();
+		}else if(pGPIOx == GPIOD)
+		{
+			GPIOD_PCLK_EN();
+		}else if(pGPIOx == GPIOE)
+		{
+			GPIOE_PCLK_EN();
+		}else if(pGPIOx == GPIOF)
+		{
+			GPIOF_PCLK_EN();
+		}else if(pGPIOx == GPIOG)
+		{
+			GPIOG_PCLK_EN();
+		}else if(pGPIOx == GPIOH)
+		{
+			GPIOH_PCLK_EN();
+		}else if(pGPIOx == GPIOI)
+		{
+			GPIOI_PCLK_EN();
+		}
+	}else
+	{
+		//TODO
+	}
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_Init
+ *
+ * @brief             - Configure the GPIOx port and Pin (Mode, Speed, PuPd, OpType and Alt Functionaity
+ *
+ * @param[in]         -
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
+{
+	uint32_t temp = 0;	//temp. register
+	//1. Configure the mode of the GPIO Pin
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG)
+	{
+		//Non Interrupts modes
+		temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+		pGPIOHandle->pGPIOx->MODER &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber); // Clear bit positions when later want to set
+		pGPIOHandle->pGPIOx->MODER |= temp; // Set bit position to the desired configuration
+
+	}else
+	{
+		//Interrup modes
+		// To be done latter
+	}
+	temp = 0;
+
+	//2. Configure the speed
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinSpeed << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->OSPEEDR &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber); // Clear bit positions when later want to set
+	pGPIOHandle->pGPIOx->OSPEEDR |= temp;// Set bit position to the desired configuration
+	temp = 0;
+
+	//3. Configure the pupd settings
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinPuPdControl << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->PUPDR &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber); // Clear bit positions when later want to set
+	pGPIOHandle->pGPIOx->PUPDR |= temp;// Set bit position to the desired configuration
+	temp = 0;
+
+	//4. Configure the OpType
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinOPType << (1 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->OTYPER &= ~(0x1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber); // Clear bit positions when later want to set
+	pGPIOHandle->pGPIOx->OTYPER |= temp;// Set bit position to the desired configuration
+	temp = 0;
+
+	//5. Configure the alt functionality
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_ALT)
+	{
+		uint8_t temp1, temp2; //temp1: to select between AFR[0] low reg, or AFR[1] High reg. temp2 to know how many bits to shift
+		temp1 = (pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 8);
+		temp2 = (pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber % 8);
+		pGPIOHandle->pGPIOx->AFR[temp1] &= (0xF << (4 * temp2));
+		pGPIOHandle->pGPIOx->AFR[temp1] |= (pGPIOHandle->GPIO_PinConfig.GPIO_PinAltFunMode << (4 * temp2));
+	}
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_DeInit
+ *
+ * @brief             - Reset the given GPIOx port
+ *
+ * @param[in]         - GPIO Port
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
+{
+	if(pGPIOx == GPIOA)
+	{
+		GPIOA_REG_RESET();
+	}else if(pGPIOx == GPIOB)
+	{
+		GPIOB_REG_RESET();
+	}else if(pGPIOx == GPIOC)
+	{
+		GPIOC_REG_RESET();
+	}else if(pGPIOx == GPIOD)
+	{
+		GPIOD_REG_RESET();
+	}else if(pGPIOx == GPIOE)
+	{
+		GPIOE_REG_RESET();
+	}else if(pGPIOx == GPIOF)
+	{
+		GPIOF_REG_RESET();
+	}else if(pGPIOx == GPIOG)
+	{
+		GPIOG_REG_RESET();
+	}else if(pGPIOx == GPIOH)
+	{
+		GPIOH_REG_RESET();
+	}else if(pGPIOx == GPIOI)
+	{
+		GPIOI_REG_RESET();
+	}
+}
+
+/*
+ * Data read and write
+ */
+
+/*********************************************************************
+ * @fn      		  - GPIO_ReadFromInputPin
+ *
+ * @brief             - Red input value from desired pin
+ *
+ * @param[in]         - GPIO Port
+ * @param[in]         - Pin Number
+ * @param[in]         -
+ *
+ * @return            -  reading from desired Pin Number (0 or 1)
+ *
+ * @Note              -  none
+
+ */
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
+{
+	uint8_t value;
+	//Right-shift n times (number of desired pin) to bit 0. Then mask the rest with 0
+	value = (uint8_t)((pGPIOx->IDR >> PinNumber) & 0x00000001);
+	return value;
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_ReadFromInputPort
+ *
+ * @brief             - Red input value from entire port
+ *
+ * @param[in]         - GPIO Port
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -  reading from desired Port (16 bit)
+ *
+ * @Note              -  none
+
+ */
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx)
+{
+	uint16_t value;
+	//Right-shift n times (number of desired pin) to bit 0. Then mask the rest with 0
+	value = (uint16_t)(pGPIOx->IDR);
+	return value;
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_WriteToOutputPin
+ *
+ * @brief             - Write to desired Pin
+ *
+ * @param[in]         - GPIO Port
+ * @param[in]         - Pin Number
+ * @param[in]         - Value (0 or 1)
+ *
+ * @return            -
+ *
+ * @Note              -  none
+
+ */
+void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Value)
+{
+	if(Value == GPIO_PIN_SET)
+	{
+		//Write 1 to the output data register at the bit field corresponding pin number
+		pGPIOx->ODR |= (1 << PinNumber);
+	}else
+	{
+		//Write 0
+		pGPIOx->ODR &= ~(1 << PinNumber);
+	}
+
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_WriteToOutputPort
+ *
+ * @brief             - Write to desired PORT
+ *
+ * @param[in]         - GPIO Port
+ * @param[in]         - PORT Value for all pins
+ * @param[in]         -
+ *
+ * @return            -
+ *
+ * @Note              -  none
+
+ */
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value)
+{
+	pGPIOx->ODR = Value;
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_ToggleOutputPin
+ *
+ * @brief             - Toggle desired Pin Number
+ *
+ * @param[in]         - GPIO Port
+ * @param[in]         - Pin Number
+ * @param[in]         -
+ *
+ * @return            -
+ *
+ * @Note              -  none
+
+ */
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
+{
+	pGPIOx->ODR ^= (1 << PinNumber);
+}
+
+/*
+ * IRQ Configuration and ISR handling
+ */
+void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi)
+{
+
+}
+void GPIO_IRQHandling(uint8_t PinNumber)
+{
+
+}
