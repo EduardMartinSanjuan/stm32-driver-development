b35bc9301b6a0c6836666fd562bdb6813901387a
EduardMartinSanjuan <eduard.martin.sanjuan2@gmail.com>
2025-02-07 16:37:51 +0100

    finishing GPIO exercises

--- Debug/Src/005button_interrupt.d
+++ Debug/Src/005button_interrupt.d
@@ -1,4 +1,4 @@
-Src/005button_interruptTEST.o: ../Src/005button_interruptTEST.c \
+Src/005button_interrupt.o: ../Src/005button_interrupt.c \
  C:/Users/F59EMS0/Desktop/STM32/Embedded-C/My_workspace/target/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
  C:/Users/F59EMS0/Desktop/STM32/Embedded-C/My_workspace/target/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
  C:/Users/F59EMS0/Desktop/STM32/Embedded-C/My_workspace/target/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
--- Src/005button_interrupt.c
+++ Src/005button_interrupt.c
@@ -1,14 +1,23 @@
 #include "stm32f407xx.h"
+#include <string.h>
 
 #define HIGH 1
 #define LOW 0
 #define BTN_PRESSED LOW
 
+void delay (void)
+{
+	//This will introduce ~200ms delay when sysclk is 16MHz
+	for(uint32_t i = 0; i < 500000/2; i++);
+}
 
 int main(void)
 {
 
 	GPIO_Handle_t GpioLed, GPIO_button;
+
+	memset(&GpioLed,0,sizeof(GpioLed)); //Initialize each member element to 0 (to avoid random values if any parameter is not set in the application code)
+	memset(&GPIO_button,0,sizeof(GPIO_button));
 
 	//this is led gpio configuration
 
@@ -24,13 +33,13 @@ int main(void)
 	GPIO_Init(&GpioLed);
 
 	//Button configuration
-	GPIO_button.pGPIOx = GPIOA;
-	GPIO_button.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+	GPIO_button.pGPIOx = GPIOD;
+	GPIO_button.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
 	GPIO_button.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IT_FT;
 	GPIO_button.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
-	GPIO_button.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	GPIO_button.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PU;
 
-	GPIO_PeriClockControl(GPIOA,ENABLE);
+	GPIO_PeriClockControl(GPIOD,ENABLE);
 
 	GPIO_Init(&GPIO_button);
 
@@ -43,6 +52,7 @@ int main(void)
 
 void EXTI9_5_IRQHandler (void)
 {
-	GPIO_IRQHandling(GPIO_PIN_NO_0);
+	delay(); //200ms delay
+	GPIO_IRQHandling(GPIO_PIN_NO_5);
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
 }
